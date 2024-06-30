4dc6ef91159c10fb20c0cd1bbc0035c6be2e6129
fadlullan <fadlullahherifarid@gmail.com>
2024-06-30 18:10:13 +0800

    Initial commit

--- app/src/main/java/com/example/tokojahit/TutorialActivity.java
+++ app/src/main/java/com/example/tokojahit/TutorialActivity.java
@@ -1,24 +1,32 @@
 package com.example.tokojahit;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class TutorialActivity extends AppCompatActivity {
+    private WebView webViewUkur, webViewPola;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tutorial);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // WebView untuk cara mengukur
+        webViewUkur = findViewById(R.id.wvCaraMengukur);
+        WebSettings webSettingsUkur = webViewUkur.getSettings();
+        webSettingsUkur.setJavaScriptEnabled(true);
+        webViewUkur.setWebViewClient(new WebViewClient());
+        webViewUkur.loadUrl("https://youtube.com/watch?v=MFzoOidChFk&si=epXbgGPr-VtQXBjL");
+
+        // WebView untuk cara membuat pola
+        webViewPola = findViewById(R.id.wvCaraMembuatPola);
+        WebSettings webSettingsPola = webViewPola.getSettings();
+        webSettingsPola.setJavaScriptEnabled(true);
+        webViewPola.setWebViewClient(new WebViewClient());
+        webViewPola.loadUrl("https://youtube.com/watch?v=BCyjr6xvFU8&si=ZYIeclRBeDom_yFs");
     }
 }
